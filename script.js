2eb4db9961c40c1423a5a9947b739694566cfd3a
Stephan Frolov <stefro97@yandex.ru>
2023-06-18 16:02:28 +0300

    Final

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+let number = 0;
+const count = document.querySelector(".count");
+const addBut = document.querySelector(".add");
+const subtractBut = document.querySelector(".subtract");
+const resetBut = document.querySelector(".reset");
+
+subtractBut.addEventListener("click", () => {
+  count.innerHTML = --number;
+});
+addBut.addEventListener("click", () => {
+  count.innerHTML = ++number;
+});
+resetBut.addEventListener("click", () => {
+  number = 0;
+  count.innerHTML = number;
+});
